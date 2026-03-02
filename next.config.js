81352b5ee3800fbd2b0e555ad0dd4b5eccf300aa
Test <test@example.com>
2026-03-02 18:38:50 +0200

    refactor: remove `typescript.ignoreBuildErrors` from Next.js configuration

--- next.config.js
+++ next.config.js
@@ -1,10 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    typescript: {
-        // Dangerously allow production builds to succeed even if there are type errors.
-        // Type errors are caught by CI lint checks instead.
-        ignoreBuildErrors: true,
-    },
-};
+const nextConfig = {}
 
-module.exports = nextConfig;
+module.exports = nextConfig
